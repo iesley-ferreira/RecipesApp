8413daf5b980d92934124ea24765e22eb9d14db5
Vinicius Silva Virgilli <vinicius.virgilli3@gmail.com>
2023-07-10 18:18:31 -0300

    feat(Requisito 9): implementa botão para mostrar a barra de pesquisa

--- src/components/SearchIcon.jsx
+++ src/components/SearchIcon.jsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
+
 function SeachIcon() {
+  const [showInput, setShowInput] = useState(false);
+
+  const handleInput = () => {
+    setShowInput(!showInput);
+  };
+
   return (
     <div className="search">
-      <img src="searchIcon.svg" alt="search icon" data-testid="search-top-btn" />
+      <button onClick={ handleInput }>
+        {showInput && <input type="text" data-testid="search-input" />}
+        <img src="searchIcon.svg" alt="search icon" data-testid="search-top-btn" />
+      </button>
     </div>
   );
 }
