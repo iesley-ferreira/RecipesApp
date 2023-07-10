9e2c82089941176b7844e27a4dbfd638fddb2bde
Vinicius Silva Virgilli <vinicius.virgilli3@gmail.com>
2023-07-10 18:32:28 -0300

    feat(requisito 10): implementa radio-buttons, e o teste ficará por último

--- src/components/SearchIcon.jsx
+++ src/components/SearchIcon.jsx
@@ -7,12 +7,28 @@ function SeachIcon() {
     setShowInput(!showInput);
   };
 
+  const inputs = (
+    <div>
+      <input type="text" data-testid="search-input" />
+
+      <label htmlFor="ingredient">Ingrediente</label>
+      <input type="radio" data-testid="ingredient-search-radio" id="ingredient" />
+
+      <label htmlFor="name">Nome</label>
+      <input type="radio" data-testid="name-search-radio" id="name" />
+
+      <label htmlFor="first-letter">Primeira letra</label>
+      <input type="radio" data-testid="first-letter-search-radio" id="first-letter" />
+      <button type="button" data-testid="exec-search-btn">Buscar</button>
+    </div>
+  );
+
   return (
     <div className="search">
       <button onClick={ handleInput }>
-        {showInput && <input type="text" data-testid="search-input" />}
         <img src="searchIcon.svg" alt="search icon" data-testid="search-top-btn" />
       </button>
+      {showInput ? inputs : null}
     </div>
   );
 }
