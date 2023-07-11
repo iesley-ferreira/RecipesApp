aea76a8907e5d8f6011d2dbd896ef05419cabdb4
Lira Guedes <liracrg@gmail.com>
2023-07-11 13:51:19 -0300

    fix: resolvendo problemas  req 6

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,34 +1,28 @@
 import React from 'react';
-import { screen, userEvent } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Login from '../pages/Login';
 import renderWithRouter from '../helpers/renderWithRouter';
 
+const EMAILINPUT = 'email-input';
+const PASSINPUT = 'password-input';
+
 describe('Testes page Login', () => {
-  const email = screen.getByTestId('email-input');
-  const password = screen.getByTestId('password-input');
-  const button = screen.getByRole('button', { name: /login/i });
-
-  // test('', () => {
-  //   renderWithRouter(<Login />);
-
-  //   expect(button).toBeDisabled();
-  //   act(() => userEvent.type(email, 'testo@test.com'));
-  //   expect(button).toBeDisabled();
-  //   act(() => userEvent.type(password, 'abcd1234'));
-  //   expect(button).toBeEnabled();
-  // });
-
   test('redireciona para /meals após o login bem-sucedido', () => {
     const { history } = renderWithRouter(<Login />);
-
-    userEvent.change(email, { target: { value: 'example@example.com' } });
-    userEvent.change(password, { target: { value: 'senha1234' } });
+    const email = screen.getByTestId(EMAILINPUT);
+    const password = screen.getByTestId(PASSINPUT);
+    const button = screen.getByRole('button', { name: /login/i });
+    userEvent.type(email, 'example@example.com');
+    userEvent.type(password, 'senha1234');
     userEvent.click(button);
     expect(history.location.pathname).toBe('/meals');
   });
   test('renderiza corretamente', () => {
     renderWithRouter(<Login />);
-
+    const email = screen.getByTestId(EMAILINPUT);
+    const password = screen.getByTestId(PASSINPUT);
+    const button = screen.getByRole('button', { name: /login/i });
     expect(email).toBeInTheDocument();
     expect(password).toBeInTheDocument();
     expect(button).toBeInTheDocument();
@@ -36,17 +30,21 @@ describe('Testes page Login', () => {
 
   test('habilita o botão de login quando o email e a senha são válidos', () => {
     renderWithRouter(<Login />);
-
-    userEvent.change(email, { target: { value: 'test@test.com' } });
-    userEvent.change(password, { target: { value: 'senha1234' } });
+    const email = screen.getByTestId(EMAILINPUT);
+    const password = screen.getByTestId(PASSINPUT);
+    const button = screen.getByRole('button', { name: /login/i });
+    userEvent.type(email, 'test@test.com');
+    userEvent.type(password, 'senha1234');
     expect(button).not.toBeDisabled();
   });
 
   test('desabilita o botão de login quando o email ou a senha são inválidos', () => {
     renderWithRouter(<Login />);
-
-    userEvent.change(email, { target: { value: 'emailnvalido' } });
-    userEvent.change(password, { target: { value: '123' } });
+    const email = screen.getByTestId(EMAILINPUT);
+    const password = screen.getByTestId(PASSINPUT);
+    const button = screen.getByRole('button', { name: /login/i });
+    userEvent.type(email, 'emailnvalido');
+    userEvent.type(password, '123');
     expect(button).toBeDisabled();
   });
 });
