70c2add15eba2093a98cc6e4611c770501935e79
iesley-ferreira <iesley_ferreira@hotmail.com>
2023-07-12 18:50:00 -0300

    feat: implementa pagina DoneRecipes os botoes de filtros e a logica para filtrar

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,10 +1,79 @@
+import { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import DoneRecipeCard from '../components/DoneRecipeCard';
+import iconeBebida from '../images/iconeBebida.png';
 
 function DoneRecipes() {
+  const [doneRecipesData, setDoneRecipesData] = useState([]);
+  const [fillDoneRecipes, setFillDoneRecipes] = useState([]);
+
+  useEffect(() => {
+    const doneRecipesLocalStorage = localStorage.getItem('doneRecipes');
+    const doneRecipesDataParsed = JSON.parse(doneRecipesLocalStorage);
+    setDoneRecipesData(doneRecipesDataParsed);
+  }, []);
+
+  const filterByCategory = (category) => {
+    if (category === 'all') {
+      setFillDoneRecipes(doneRecipesData);
+    } else {
+      const filteredRecipes = doneRecipesData
+        .filter((recipe) => recipe.type === category);
+      setFillDoneRecipes(filteredRecipes);
+    }
+  };
+
   return (
     <div>
       <Header />
-      <h1>Done Recipes</h1>
+      <div className="doneRecipes-categories-container">
+        <button
+          data-testid="filter-by-all-btn"
+          type="button"
+          name="all"
+          onClick={ ({ name }) => filterByCategory(name) }
+          src={ iconeBebida }
+        >
+          All
+        </button>
+        <button
+          data-testid="filter-by-meal-btn"
+          type="button"
+          onClick={ ({ name }) => filterByCategory(name) }
+          src={ iconeBebida }
+        >
+          Meals
+        </button>
+        <button
+          data-testid="filter-by-drink-btn"
+          type="button"
+          onClick={ ({ name }) => filterByCategory(name) }
+          src={ iconeBebida }
+        >
+          Drinks
+        </button>
+      </div>
+      <div className="doneRecipes-card-container">
+        {
+          fillDoneRecipes.length === 0 ? (
+            doneRecipesData.map((recipe, index) => (
+              <DoneRecipeCard
+                key={ index }
+                recipe={ recipe }
+                index={ index }
+              />
+            ))
+          ) : (
+            fillDoneRecipes.map((recipe, index) => (
+              <DoneRecipeCard
+                key={ index }
+                recipe={ recipe }
+                index={ index }
+              />
+            ))
+          )
+        }
+      </div>
     </div>
   );
 }
