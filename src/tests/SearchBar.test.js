973b39c75ab88011bdbb0ee91a81e63f08c9aff8
Lira Guedes <liracrg@gmail.com>
2023-07-14 21:16:19 -0300

    test: Realizando testes Recipes e Header

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -6,24 +6,26 @@ import renderWithRouter from '../helpers/renderWithRouter';
 import SearchBar from '../components/SearchBar';
 import { fetchRecipesByIngredient, fetchRecipesByName, fetchRecipesByFirstLetter } from '../services/fetchAPI';
 
+const searchBtn = 'exec-search-btn';
+
 describe('Teste a Página SearchBar', () => {
   test('exibe input de busca ao clicar no ícone de busca', () => {
     renderWithRouter(<SearchBar />);
     expect(screen.queryByTestId('search-input')).toBeNull();
     userEvent.click(getByTestId('search-icon'));
 
-    expect(screen.getByLabelText('Search')).toBeInTheDocument();
+    expect(screen.queryByTestId('search-input')).toBeInTheDocument();
     expect(screen.getByTestId('ingredient-search-radio')).toBeInTheDocument();
     expect(screen.getByTestId('name-search-radio')).toBeInTheDocument();
     expect(screen.getByTestId('first-letter-search-radio')).toBeInTheDocument();
-    // expect(screen.getByTestId('exec-search-btn')).toBeInTheDocument();
+    expect(screen.getByTestId(searchBtn)).toBeInTheDocument();
   });
 
   test('faz a busca por ingrediente', async () => {
     renderWithRouter(<SearchBar />);
 
     await act(async () => {
-      // userEvent.click(screen.getByTestId('exec-search-btn'));
+      // userEvent.click(screen.getByTestId(searchBtn));
     });
 
     expect(fetchRecipesByIngredient).toHaveBeenCalledWith(ingredientMock);
@@ -37,7 +39,7 @@ describe('Teste a Página SearchBar', () => {
     fetchRecipesByName.mockResolvedValueOnce('receitas por nome');
 
     await act(async () => {
-    //   userEvent.click(getByTestId('exec-search-btn'));
+    //   userEvent.click(getByTestId(searchBtn));
     });
 
     expect(screen.fetchRecipesByName).toHaveBeenCalledWith(nameMock);
@@ -51,7 +53,7 @@ describe('Teste a Página SearchBar', () => {
     fetchRecipesByFirstLetter.mockResolvedValueOnce('receitas por primeira letra');
 
     await act(async () => {
-      userEvent.click(screen.getByTestId('exec-search-btn'));
+      userEvent.click(screen.getByTestId(searchBtn));
     });
 
     expect(fetchRecipesByFirstLetter).toHaveBeenCalledWith(letterMock);
@@ -63,7 +65,7 @@ describe('Teste a Página SearchBar', () => {
 
     global.alert = jest.fn();
 
-    userEvent.click(getByTestId('exec-search-btn'));
+    userEvent.click(getByTestId(searchBtn));
 
     expect(global.alert).toHaveBeenCalledTimes(1);
   });
