ceaa2561b48d1a32e83dc42a49a5d480fd3b21f5
Lira Guedes <liracrg@gmail.com>
2023-07-17 19:49:33 -0300

    merge in search branc

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 // import Profile from '../pages/Profile';
 import { act } from 'react-dom/test-utils';
