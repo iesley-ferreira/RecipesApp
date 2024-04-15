43a506eeceead437b8b699275d8c463234d613f2
iesley-ferreira <iesley_ferreira@hotmail.com>
2024-04-15 18:10:02 -0300

    new styling for the app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,88 +1,82 @@
-import { useContext } from 'react';
-import { useHistory } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom/cjs/react-router-dom';
-import profileIcon from '../images/iconePerfil.png';
-import './styles/Header.css';
-import logoRecipes from '../images/logoRecipes.png';
-import iconeRecipes2 from '../images/iconeRecipes2.png';
-import searchIcon from '../images/iconePesquisar.png';
-import receitasContext from '../context/ReceitasContext';
-import PageDrink from '../images/PageDrink.png';
-import iconePrato from '../images/iconePrato.png';
-import Favorites from '../images/Favorites.png';
-import DoneRecipes from '../images/DoneRecipes.png';
+import PropTypes from "prop-types";
+import { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom/cjs/react-router-dom";
+import receitasContext from "../context/ReceitasContext";
+import profileIcon from "../images/iconePerfil.png";
+import searchIcon from "../images/iconePesquisar.png";
+import iconeRecipes2 from "../images/iconeRecipes2.png";
+import logoRecipes from "../images/logoRecipes.png";
+import "./styles/Header.css";
 
 function Header(props) {
   const { showBtn } = props;
-  const { pathname } = window.location;
+  // const { pathname } = window.location;
   const history = useHistory();
 
-  const str = pathname;
-  const novaStr = str.replace('/', '').replace(/-/g, ' ').replace(/\b\w/g, (match) => match.toUpperCase());
+  // const str = pathname;
+  // const novaStr = str
+  //   .replace("/", "")
+  //   .replace(/-/g, " ")
+  //   .replace(/\b\w/g, (match) => match.toUpperCase());
 
-  const {
-    showInput,
-    setShowInput,
-  } = useContext(receitasContext);
+  const { showInput, setShowInput } = useContext(receitasContext);
 
   const redirectToProfile = () => {
-    history.push('/profile');
+    history.push("/profile");
   };
 
   const showSearchInputs = () => {
     setShowInput(!showInput);
   };
 
-  const getHeaderImage = (title) => {
-    switch (title) {
-    case 'Meals':
-      return iconePrato;
-    case 'Drinks':
-      return PageDrink;
-    case 'Favorite Recipes':
-      return Favorites;
-    case 'Profile':
-      return profileIcon;
-    case 'Done Recipes':
-      return DoneRecipes;
-    default:
-      return '';
-    }
-  };
+  // const getHeaderImage = (title) => {
+  //   switch (title) {
+  //     case "Meals":
+  //       return iconePrato;
+  //     case "Drinks":
+  //       return PageDrink;
+  //     case "Favorite Recipes":
+  //       return Favorites;
+  //     case "Profile":
+  //       return profileIcon;
+  //     case "Done Recipes":
+  //       return DoneRecipes;
+  //     default:
+  //       return "";
+  //   }
+  // };
 
   return (
-    <div className="header-container">
-      <div className="header-superior">
-        <Link to="/meals" className="logo-container">
-          <img src={ iconeRecipes2 } alt="logo1recipes" />
-          <img src={ logoRecipes } alt="logo2recipes" />
+    <div className='header-container'>
+      <div className='header-superior'>
+        <Link to='/meals' className='logo-container'>
+          <img src={iconeRecipes2} alt='logo1recipes' />
+          <img src={logoRecipes} alt='logo2recipes' />
         </Link>
-        <header className="links-container">
-          {
-            showBtn && (
-              <input
-                type="image"
-                data-testid="search-top-btn"
-                src={ searchIcon }
-                alt="search icon"
-                onClick={ showSearchInputs }
-              />
-            )
-          }
+        <header className='links-container'>
+          {showBtn && (
+            <input
+              type='image'
+              data-testid='search-top-btn'
+              src={searchIcon}
+              alt='search icon'
+              onClick={showSearchInputs}
+            />
+          )}
           <input
-            type="image"
-            data-testid="profile-top-btn"
-            src={ profileIcon }
-            alt="profile icon"
-            onClick={ redirectToProfile }
+            type='image'
+            data-testid='profile-top-btn'
+            src={profileIcon}
+            alt='profile icon'
+            onClick={redirectToProfile}
           />
         </header>
       </div>
-      <div className="title-container">
-        <img src={ getHeaderImage(novaStr) } alt={ novaStr } />
-        <h1 data-testid="page-title">{ novaStr }</h1>
-      </div>
+      {/* <div className='title-container'>
+        <img className='done-image' src={getHeaderImage(novaStr)} alt={novaStr} />
+        <h1 data-testid='page-title'>{novaStr}</h1>
+      </div> */}
     </div>
   );
 }
--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,47 +1,52 @@
-import { useContext, useEffect, useState } from 'react';
-import { useLocation, Link } from 'react-router-dom';
-import Card from '../components/Card';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import './styles/Recipes.css';
-import receitasContext from '../context/ReceitasContext';
-import SearchBar from '../components/SearchBar';
-import Beef from '../images/mealsCategories/Beef.png';
-import All from '../images/mealsCategories/All.png';
-import Breakfast from '../images/mealsCategories/Breakfast.png';
-import Chicken from '../images/mealsCategories/Chicken.png';
-import Dessert from '../images/mealsCategories/Dessert.png';
-import Goat from '../images/mealsCategories/Goat.png';
-import Ordinary from '../images/mealsCategories/Ordinary.png';
-import Cocktail from '../images/mealsCategories/Cocktail.png';
-import Shake from '../images/mealsCategories/Shake.png';
-import Other from '../images/mealsCategories/Other.png';
-import Cocoa from '../images/mealsCategories/Cocoa.png';
+import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Card from "../components/Card";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import SearchBar from "../components/SearchBar";
+import receitasContext from "../context/ReceitasContext";
+import All from "../images/mealsCategories/All.png";
+import Beef from "../images/mealsCategories/Beef.png";
+import Breakfast from "../images/mealsCategories/Breakfast.png";
+import Chicken from "../images/mealsCategories/Chicken.png";
+import Cocktail from "../images/mealsCategories/Cocktail.png";
+import Cocoa from "../images/mealsCategories/Cocoa.png";
+import Dessert from "../images/mealsCategories/Dessert.png";
+import Goat from "../images/mealsCategories/Goat.png";
+import Ordinary from "../images/mealsCategories/Ordinary.png";
+import Other from "../images/mealsCategories/Other.png";
+import Shake from "../images/mealsCategories/Shake.png";
+import "./styles/Recipes.css";
+
+// ===================== Importação de icones ==================== //
+import DoneRecipes from "../images/DoneRecipes.png";
+import Favorites from "../images/Favorites.png";
+import PageDrink from "../images/PageDrink.png";
+import profileIcon from "../images/iconePerfil.png";
+import iconePrato from "../images/iconePrato.png";
 
 function Recipes() {
-  const location = useLocation();
-  const { pathname } = location;
+  // const { showBtn } = props;
+  // const showBtn = true;
+  const { pathname } = window.location;
+  // const history = useHistory();
+  // const location = useLocation();
 
-  const {
-    usualRecipes,
-    setUsualRecipes,
-    setCounter,
-    categories,
-    setCategories,
-  } = useContext(receitasContext);
-  const [filter, setFilter] = useState('');
+  const { usualRecipes, setUsualRecipes, setCounter, categories, setCategories } =
+    useContext(receitasContext);
+  const [filter, setFilter] = useState("");
 
-  const food = pathname === '/meals' ? 'meals' : 'drinks';
-  const idFood = pathname === '/meals' ? 'idMeal' : 'idDrink';
+  const food = pathname === "/meals" ? "meals" : "drinks";
+  const idFood = pathname === "/meals" ? "idMeal" : "idDrink";
 
   useEffect(() => {
     const fetchRecipes = async () => {
       const recipesNum = 12;
       const categoryNum = 5;
-      const path = pathname === '/meals' ? 'themeal' : 'thecocktail';
+      const path = pathname === "/meals" ? "themeal" : "thecocktail";
 
       const response = await fetch(
-        `https://www.${path}db.com/api/json/v1/1/search.php?s=`,
+        `https://www.${path}db.com/api/json/v1/1/search.php?s=`
       );
       const data = await response.json();
       const recipes = data[food].filter((meal, index) => index < recipesNum);
@@ -49,11 +54,11 @@ function Recipes() {
       setCounter(1);
 
       const responseCategory = await fetch(
-        `https://www.${path}db.com/api/json/v1/1/list.php?c=list`,
+        `https://www.${path}db.com/api/json/v1/1/list.php?c=list`
       );
       const dataCategory = await responseCategory.json();
       const filterCategory = dataCategory[food].filter(
-        (meal, index) => index < categoryNum,
+        (meal, index) => index < categoryNum
       );
 
       setCategories(filterCategory);
@@ -62,7 +67,7 @@ function Recipes() {
   }, [food, pathname, setUsualRecipes, setCounter, setCategories]);
 
   const handleAll = async () => {
-    const path = pathname === '/meals' ? 'themeal' : 'thecocktail';
+    const path = pathname === "/meals" ? "themeal" : "thecocktail";
     const num = 12;
     const response = await fetch(`https://www.${path}db.com/api/json/v1/1/search.php?s=`);
     const data = await response.json();
@@ -71,12 +76,12 @@ function Recipes() {
   };
 
   const handleCategory = async (categoryName) => {
-    const path = pathname === '/meals' ? 'themeal' : 'thecocktail';
+    const path = pathname === "/meals" ? "themeal" : "thecocktail";
     const num = 12;
 
     if (categoryName !== filter) {
       const response = await fetch(
-        `https://www.${path}db.com/api/json/v1/1/filter.php?c=${categoryName}`,
+        `https://www.${path}db.com/api/json/v1/1/filter.php?c=${categoryName}`
       );
       const data = await response.json();
       const recipes = data[food].splice(0, num);
@@ -84,83 +89,110 @@ function Recipes() {
       setFilter(categoryName);
     } else {
       handleAll();
-      setFilter('');
+      setFilter("");
     }
   };
 
   const getCategoryImage = (categoryName) => {
     switch (categoryName) {
-    case 'Beef':
-      return Beef;
-    case 'Breakfast':
-      return Breakfast;
-    case 'Chicken':
-      return Chicken;
-    case 'Dessert':
-      return Dessert;
-    case 'Goat':
-      return Goat;
-    case 'Ordinary Drink':
-      return Ordinary;
-    case 'Cocktail':
-      return Cocktail;
-    case 'Shake':
-      return Shake;
-    case 'Cocoa':
-      return Cocoa;
-    default:
-      return Other;
+      case "Beef":
+        return Beef;
+      case "Breakfast":
+        return Breakfast;
+      case "Chicken":
+        return Chicken;
+      case "Dessert":
+        return Dessert;
+      case "Goat":
+        return Goat;
+      case "Ordinary Drink":
+        return Ordinary;
+      case "Cocktail":
+        return Cocktail;
+      case "Shake":
+        return Shake;
+      case "Cocoa":
+        return Cocoa;
+      default:
+        return Other;
     }
   };
 
+  const getHeaderImage = (title) => {
+    switch (title) {
+      case "Meals":
+        return iconePrato;
+      case "Drinks":
+        return PageDrink;
+      case "Favorite Recipes":
+        return Favorites;
+      case "Profile":
+        return profileIcon;
+      case "Done Recipes":
+        return DoneRecipes;
+      default:
+        return "";
+    }
+  };
+
+  const str = pathname;
+  const novaStr = str
+    .replace("/", "")
+    .replace(/-/g, " ")
+    .replace(/\b\w/g, (match) => match.toUpperCase());
+
   return (
-    <div className="recipes-container">
-      <Header showBtn />
-      <div className="searchBar-container">
-        <SearchBar />
-      </div>
-      <div className="categories-container-foods">
-        <div className="category-item-filter">
-          {categories.length > 0 && (
-            <div className="recipes-categories-all-btn">
-              <div className="category-item-circle-foods">
+    <div className='phoneFrame'>
+      <div className='recipes-container'>
+        <Header showBtn />
+        <div className='title-container'>
+          <img className='done-image' src={getHeaderImage(novaStr)} alt={novaStr} />
+          <h1 data-testid='page-title'>{novaStr}</h1>
+        </div>
+        <div className='searchBar-container'>
+          <SearchBar />
+        </div>
+        <div className='categories-container-foods'>
+          <div className='category-item-filter'>
+            {categories.length > 0 && (
+              <div className='recipes-categories-all-btn'>
+                <div className='category-item-circle-foods'>
+                  <input
+                    type='image'
+                    src={All}
+                    alt='All'
+                    data-testid='All-category-filter'
+                    onClick={() => handleAll()}
+                  />
+                </div>
+                All
+              </div>
+            )}
+          </div>
+          {categories.map((categoryName, index) => (
+            <div key={index} className='category-item-filter'>
+              <div className='category-item-circle-foods'>
                 <input
-                  type="image"
-                  src={ All }
-                  alt="All"
-                  data-testid="All-category-filter"
-                  onClick={ () => handleAll() }
+                  type='image'
+                  src={getCategoryImage(categoryName.strCategory)}
+                  alt={categoryName.strCategory}
+                  data-testid={`${categoryName.strCategory}-category-filter`}
+                  onClick={() => handleCategory(categoryName.strCategory)}
                 />
               </div>
-              All
+              <p>{categoryName.strCategory}</p>
             </div>
-          )}
+          ))}
         </div>
-        {categories.map((categoryName, index) => (
-          <div key={ index } className="category-item-filter">
-            <div className="category-item-circle-foods">
-              <input
-                type="image"
-                src={ getCategoryImage(categoryName.strCategory) }
-                alt={ categoryName.strCategory }
-                data-testid={ `${categoryName.strCategory}-category-filter` }
-                onClick={ () => handleCategory(categoryName.strCategory) }
-              />
-            </div>
-            <p>
-              {categoryName.strCategory}
-            </p>
-          </div>
-        ))}
+        <div className='cards-container'>
+          {usualRecipes.map((recipe, index) => (
+            <Link to={`/${food}/${recipe[idFood]}`} key={recipe[idFood]}>
+              <Card option={pathname} recipe={recipe} index={index} />
+            </Link>
+          ))}
+        </div>
+        <Footer />
       </div>
-      <div className="cards-container">
-        {usualRecipes.map((recipe, index) => (
-          <Link to={ `/${food}/${recipe[idFood]}` } key={ recipe[idFood] }>
-            <Card option={ pathname } recipe={ recipe } index={ index } />
-          </Link>
-        ))}
-      </div>
-      <Footer />
     </div>
   );
 }
